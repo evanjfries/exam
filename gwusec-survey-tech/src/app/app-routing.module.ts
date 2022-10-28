d44966d27fbec3ff1c397b5d556f6a6024da2aac
Evan Fries <evanfries@gwu.edu>
2022-10-27 22:08:52 -0400

    Extension Installation Check

--- gwusec-survey-tech/src/app/app-routing.module.ts
+++ gwusec-survey-tech/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {LandingPageComponent} from './landing-page/landing-page.component';
+import { WebcamModule } from 'ngx-webcam';
 
 const routes: Routes = [
   {path: 'start', component: LandingPageComponent},
@@ -20,4 +21,7 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {
+  routes: Routes = [
+    { path: '/', component: WebcamModule},
+  ];
 }
